aec99fb2b8b1f88244fd9434367b2fb13d032413
Rathan Kumar <rathan.kumar@addepar.com>
2020-04-09 13:26:17 -0400

    Update winlibs.R
    Tst commit

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,6 +2,8 @@ if(getRversion() < "3.3.0") {
   stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
 }
 
+TEST
+
 # For details see: https://github.com/rwinlib/gdal2
 VERSION <- commandArgs(TRUE)
 if(!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))){
